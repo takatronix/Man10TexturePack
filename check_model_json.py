bddc96d8e90da4d4cd1dd49586bcde930cb6eeb8
okiraku <okiraku48@gmail.com>
2020-09-26 17:48:07 +0900

    model以下にあるjsonが参照しているtextureのリストをmodel順とtexture順でcsvに出力するpythonスクリプトを作成 modelに書かれているtextureのパスが見つからない場合はエラーメッセージで表示する

---
+++ check_model_json.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+# このファイルは、asset/minecraft以下にある
+# modelとtextureの紐づけリストを作成するためのスクリプトです。
+
+import glob
+import json
+import re
+
+# ところどころに出てくる、re.sub(r"\\", "/", ....)  は
+# Windowsで実行した際にパスの区切りが "\" になるので "/" になおしています。
+# したがって Mac / Linux 環境の場合は意味のない処理です。
+
+# ./asset/minecraft/models 以下にある全jsonファイルのリストを作成
+jsonlist = glob.glob("./assets/minecraft/models/**/*.json", recursive=True)
+
+# ./asset/minecraft/textures 以下にある全pngファイルのリストを作成
+tex_list = {}
+for texpath in glob.glob("./assets/minecraft/textures/**/*.png", recursive=True):
+    tex_filename = re.sub(r"\\", "/", re.sub(".\/assets\/minecraft\/textures\\\\", "", texpath))
+    key = re.sub("\.png$", "", tex_filename)
+    tex_list[key] = []
+
+# 各モデル(json)が参照しているtextureのリストをCSV形式で出力
+model_tex_list = open("model_tex_list.csv", "w")
+for filepath in jsonlist:
+    with open(filepath, encoding="utf-8") as f:
+        model_data = json.load(f)
+        if model_data["textures"]:
+            textures = model_data["textures"].values()
+            model_tex_list.write(re.sub(r"\\", "/", filepath) + "," + ",".join(textures) + "\n")
+
+            # 各モデル(json)に書かれているtexture名をkeyにしてtex_listにモデルのファイルパスを追加
+            for texture in textures:
+                if texture in tex_list:
+                    tex_list[texture].append(filepath)
+                else:   # tex_listに当該keyがない状態＝モデル(JSON)に書いてあるtextureへのパスが異なるまたは存在しない場合
+                    print(texture ," is not found in tex_list. please check ", filepath)
+                    tex_list[texture] = [filepath]
+
+# 各textureが参照されているモデル(json)のリストをCSV形式で出力
+tex_model_list = open("tex_model_list.csv", "w")
+for tex, path in tex_list.items():
+    if len(path) > 0:
+        tex_model_list.write(tex + "," + re.sub(r"\\", "/", ",".join(path)) + "\n")
+    else:
+        tex_model_list.write(tex + "\n")
+
